8a3d8d721ef7a6b94b82b6b56714cd35b479aee3
TESFAY\Tesfay <tesfaykidane@gmail.com>
2017-06-13 11:57:43 -0500

    spring boot hello world app

---
+++ src/main/java/edu/mum/HelloSpringThymeBootApplication.java
@@ -0,0 +1,12 @@
+package edu.mum;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HelloSpringThymeBootApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HelloSpringThymeBootApplication.class, args);
+	}
+}
---
+++ src/test/java/edu/mum/HelloSpringThymeBootApplicationTests.java
@@ -0,0 +1,13 @@
+package edu.mum;
+
+import org.junit.Test;
+/*
+@RunWith(SpringRunner.class)
+@SpringBootTest*/
+public class HelloSpringThymeBootApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
